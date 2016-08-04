8e7fd3564237bc6f4a7f5b8c8204eac672f92ad6
Jonathan Guthrie <jono@perfect.org>
2016-08-04 12:05:52 -0400

    adding zip dep

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
 	name: "PerfectDocGenerator",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect.git", versions: Version(0,0,0)..<Version(10,0,0))
+		.Package(url: "https://github.com/PerfectlySoft/Perfect.git", versions: Version(0,0,0)..<Version(10,0,0)),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Zip.git", versions: Version(0,0,0)..<Version(10,0,0))
     ]
 )
